47a968232fff3a05b0e973f2d0ff8ab5b586bf97
João Favato <favato.joao@gmailcom>
2026-04-08 23:04:14 -0300

    1.1 Hello World Zephyr (GPIO Básico)

--- src/main.c
+++ src/main.c
@@ -2,77 +2,40 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-#define SLEEP_TIME_MS1 5000
-#define SLEEP_TIME_MS2 1000
+#define SLEEP_TIME_MS 1000
 
+// Define o LED usando Device Tree
 #define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-#define LED2_NODE DT_ALIAS(led2)
 
+// Verifica se o LED está definido no Device Tree
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #else
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
-
-#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-#else
-#error "Unsupported board: led1 devicetree alias is not defined"
-#endif
-#if DT_NODE_HAS_STATUS(LED2_NODE, okay)
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
-#else
-#error "Unsupported board: led2 devicetree alias is not defined"
-#endif
-
-typedef enum {
-    STATE_GREEN,
-    STATE_YELLOW,
-    STATE_RED
-} state_t;
 
 void main(void)
 {
     int ret;
 
-    if (!gpio_is_ready_dt(&led0)) {
-        printk("Error: LED device %s is not ready\n", led0.port->name);
+    // Verifica se o device está pronto
+    if (!gpio_is_ready_dt(&led)) {
+        printk("Error: LED device %s is not ready\n", led.port->name);
         return;
     }
 
-    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
-    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
-    ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
-    
+    // Configura o pino como saída
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         printk("Error %d: failed to configure LED pin\n", ret);
         return;
     }
 
-    printk("LED blinking on %s pin %d\n", led0.port->name, led0.pin);
-    printk("LED blinking on %s pin %d\n", led1.port->name, led1.pin);
-
-    state_t state = STATE_GREEN;
+    printk("LED blinking on %s pin %d\n", led.port->name, led.pin);
 
     while (1) {
-        switch(state){
-        case STATE_GREEN:
-            gpio_pin_set_dt(&led2, 0);
-            gpio_pin_set_dt(&led0, 1);
-            k_msleep(SLEEP_TIME_MS1);
-            state = STATE_YELLOW;
-            break;
-        case STATE_YELLOW:
-            gpio_pin_set_dt(&led2, 1);
-            k_msleep(SLEEP_TIME_MS2);
-            state = STATE_RED;
-            break;
-        case STATE_RED:
-            gpio_pin_set_dt(&led0, 0);
-            k_msleep(SLEEP_TIME_MS1);
-            state = STATE_GREEN;
-            break;
-    }
+        // Toggle do LED usando a nova API
+        gpio_pin_toggle_dt(&led);
+        k_msleep(SLEEP_TIME_MS);
     }
 }
