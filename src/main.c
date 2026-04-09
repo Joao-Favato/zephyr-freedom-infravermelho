9ce8e7b9a9d5400bbdc6dffefd4d8f6be3b20cdc
João Favato <favato.joao@gmailcom>
2026-04-08 23:45:16 -0300

    4.1 Teste Motor Único

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ uint16_t duty_50  = TPM_MODULE/2;       // 50% de duty cycle (meio brilho)
 uint16_t duty_100  = TPM_MODULE;       // 100% de duty cycle
 uint16_t duty_25  = TPM_MODULE/4;       // 25% de duty cycle
 
+typedef enum {
+    ESTADO_25,
+    ESTADO_50,
+    ESTADO_100
+} estado_t;
 
 int main(void)
 {
@@ -25,23 +30,52 @@ int main(void)
 
 
     // Inicializa o canal do TPM2 para gerar sinal PWM nas portas GPIOB_2 e GPIOB_3
-    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18);
-    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 2);
 
 
-   
+    // Define o valor do duty cycle
+    pwm_tpm_CnV(TPM2, 0, duty_25);
+
+
+    //Configuração dos pinos para saída digital
+    gpio_pin_configure(gpio_b, 0, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_b, 1, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_a, 4, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_a, 5, GPIO_OUTPUT);
+
+
+    //Definição dos estados iniciais dos pinos
+    gpio_pin_set(gpio_b, 0, 1);
+    gpio_pin_set(gpio_b, 1, 0);
+    gpio_pin_set(gpio_a, 4, 0);
+    gpio_pin_set(gpio_a, 5, 1);
+
+    estado_t estado = ESTADO_25;
+
     // Loop infinito
     for (;;)
     {
-        pwm_tpm_CnV(TPM2, 0, duty_25);
-        pwm_tpm_CnV(TPM2, 1, duty_50);
-       k_sleep(K_MSEC(1000));
-       pwm_tpm_CnV(TPM2, 0, duty_100);
-       pwm_tpm_CnV(TPM2, 1, duty_100);
-       k_sleep(K_MSEC(1000));
+       switch (estado)
+        {
+            case ESTADO_25:
+                pwm_tpm_CnV(TPM2, 0, duty_25);
+                k_msleep(3000);
+                estado = ESTADO_50;
+                break;
+
+            case ESTADO_50:
+                pwm_tpm_CnV(TPM2, 0, duty_50);
+                k_msleep(3000);
+                estado = ESTADO_100;
+                break;
+
+            case ESTADO_100:
+                pwm_tpm_CnV(TPM2, 0, duty_100);
+                k_msleep(3000);
+                estado = ESTADO_25;
+                break;
+        }
     }
     return 0;
 }
     
-
-
