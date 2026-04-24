f4f196bdbd4c8da85614fd05fbf4d4e766ab0d2f
João Favato <favato.joao@gmailcom>
2026-04-24 19:54:03 -0300

    Robô seguidor de linha

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# zephyr-freedom-infravermelho
-Implementação do sensor infravermelho
+# Zephyr-Freedom-Infravermelho
+
+## 1. Teste do sensor infravermelho
+Antes de implementar os sensores no conjunto do carro, primeiramente fiz testes com os sensores utilizando os leds da placa para certificar a funcionalidade deles. Dessa maneira, fiz o teste abaixo com superfícies pretas e de outras cores.
+
+<img width="394" height="700" alt="TesteDosSensoresComLED" src="evidencias/TesteDosSensoresComLED.gif" />
+
+
+## 2. Implementação de 1 sensor no carro
+Tendo testado os sensores infravermelho, testei novamente os movimentos dos motores. Depois de devido atestado o funcionamento de todos os componentes, implementei um único sensor na frente do carrinho. Nessa nova configuração, o carrinho liga os seus dois motores enquanto estiver passando sobre uma superfície preta, como mostrado no vídeo.
+
+<img width="394" height="700" alt="CarroCom1Sensor" src="evidencias\CarroCom1Sensor.gif" />
+
+
+## 3. Robô seguidor de linha
+Depois de conseguir fazer a implementação do segundo sensor, fui fazendo diversos ajustes nos níveis de velocidade dependendo do input capturado. A versão atual, mais próxima do projeto final do robô seguidor de linha, usa um "start" para fazer os motores girarem uma primeira vez e depois mantém uma velocidade mais baixa para permitir a realização das curvas com maior estabilidade. 
+
+<img width="394" height="700" alt="CarroCom1Sensor" src="evidencias/RobôSeguidorDeLinha.gif" />
--- src/main.c
+++ src/main.c
@@ -12,6 +12,16 @@ const struct device *gpio_e =  DEVICE_DT_GET(DT_NODELABEL(gpioe));
 uint16_t duty_50  = TPM_MODULE/2;       // 50% de duty cycle (meio brilho)
 uint16_t duty_100  = TPM_MODULE;       // 100% de duty cycle
 uint16_t duty_25  = TPM_MODULE/4;       // 25% de duty cycle
+uint16_t duty_75  = TPM_MODULE * 3/4;   // 75% de duty cycle
+uint16_t duty_0  = TPM_MODULE * 0;   // 0% de duty cycle
+uint16_t duty_10  = TPM_MODULE/10;       // 10% de duty cycle (meio brilho)
+uint16_t duty_20  = TPM_MODULE/5;       // 20% de duty cycle (meio brilho)
+uint16_t duty_30  = TPM_MODULE * 0.3;       // 30% de duty cycle (meio brilho)
+uint16_t duty_40  = TPM_MODULE * 0.4;       // 40% de duty cycle (meio brilho)
+uint16_t duty_60  = TPM_MODULE * 0.6;       // 50% de duty cycle (meio brilho)
+uint16_t duty_70  = TPM_MODULE * 0.7;       // 50% de duty cycle (meio brilho)
+uint16_t duty_80  = TPM_MODULE * 0.8;       // 50% de duty cycle (meio brilho)
+uint16_t duty_90  = TPM_MODULE * 0.9;       // 50% de duty cycle (meio brilho)
 
 typedef enum {
     ESTADO_25,
@@ -21,7 +31,7 @@ typedef enum {
 
 int main(void)
 {  
-    int val1;
+    int val1, val2;
        
     // Inicializa o módulo TPM2 com:
     // - base do TPMx
@@ -30,8 +40,8 @@ int main(void)
     // - tipo de clock (TPM_CLK)
     // - prescaler de 1 a 128 (PS)
     // - modo de operação EDGE_PWM
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
-    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_64, EDGE_PWM);
+    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_64, EDGE_PWM);
    
     // Inicializa o canal do TPM2 para gerar sinal PWM nas portas GPIOB_2 e GPIOB_3
     pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 2);
@@ -39,6 +49,7 @@ int main(void)
 
 
     // Define o valor do duty cycle
+    // Canal 0 = PTB2, Canal 1 = PTB3
     pwm_tpm_CnV(TPM2, 0, duty_100);
     pwm_tpm_CnV(TPM2, 1, duty_100);
 
@@ -49,6 +60,8 @@ int main(void)
     gpio_pin_configure(gpio_a, 5, GPIO_OUTPUT);
 
     //Configuração dos pinos para entrada digital
+    // Sensor esquerda: Porta E5
+    // Sensor direita: Porta E4
     gpio_pin_configure(gpio_e, 4, GPIO_INPUT | GPIO_PULL_UP);
     gpio_pin_configure(gpio_e, 5, GPIO_INPUT | GPIO_PULL_UP);
 
@@ -57,22 +70,36 @@ int main(void)
     //// IN1 = B0 e IN2 = B1
     ///Motor direita: Portas A4 e A5
     //// IN3 = A4 e IN4 = A5
-    gpio_pin_set(gpio_b, 0, 0);
+    gpio_pin_set(gpio_b, 0, 1);
     gpio_pin_set(gpio_b, 1, 0);
-    gpio_pin_set(gpio_a, 4, 0);
+    gpio_pin_set(gpio_a, 4, 1);
     gpio_pin_set(gpio_a, 5, 0);
 
+    pwm_tpm_CnV(TPM2, 0, duty_100);
+    pwm_tpm_CnV(TPM2, 1, duty_100);
+    k_msleep(15);
     // Loop infinito
     for (;;){  
-        val1 = gpio_pin_get(gpio_e, 4); // Leitura do sensor 1
-        //// Carro anda com um sensor quando ele estiver em cima da linha preta
-        if (val1 == 1){
-            gpio_pin_set(gpio_b, 0, 1);
-            gpio_pin_set(gpio_a, 4, 1);
-        } else {
-            gpio_pin_set(gpio_b, 0, 0);
-            gpio_pin_set(gpio_a, 4, 0);
+        /// = 1 quando o sensor estiver sobre a linha preta; = 0 quando estiver fora da linha preta
+        val1 = gpio_pin_get(gpio_e, 4); // Leitura do sensor direita
+        val2 = gpio_pin_get(gpio_e, 5); // Leitura do sensor esquerda
+                
+        if (val1 == 0 && val2 == 0){ // Linha reta            
+            pwm_tpm_CnV(TPM2, 0, duty_60);
+            pwm_tpm_CnV(TPM2, 1, duty_60);
+        } else if (val1 == 0 && val2 == 1) { // Curva para a direita            
+            pwm_tpm_CnV(TPM2, 0, duty_60);
+            pwm_tpm_CnV(TPM2, 1, duty_0);
+        } else if (val1 == 1 && val2 == 0){ // Curva para a esquerda           
+            pwm_tpm_CnV(TPM2, 0, duty_0);
+            pwm_tpm_CnV(TPM2, 1, duty_60);
+        } else {    
+            pwm_tpm_CnV(TPM2, 0, duty_60);
+            pwm_tpm_CnV(TPM2, 1, duty_60);
         }
+            k_msleep(5); // Aguarda 10 ms antes da próxima leitura dos sensores
+
+
     }
     return 0;
 }
